e92b148ea7969d98bddc087ec3e1f3cdb574f0af
flanux <forlinux40@gmail.com>
2026-09-22 09:35:32 +0545

    ic

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Bizbox.Data;
+using Bizbox.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,10 +14,32 @@ public class HomeController : Controller
         _db = db;
     }
 
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(int? category, string? q)
     {
         var businessTypes = await _db.BusinessTypes.ToListAsync();
-        return View(businessTypes);
+
+        var productsQuery = _db.Products
+            .Include(p => p.BusinessType)
+            .Where(p => p.IsActive && p.SellerType == ListingSellerType.Platform)
+            .AsQueryable();
+
+        if (category.HasValue)
+        {
+            productsQuery = productsQuery.Where(p => p.BusinessTypeId == category.Value);
+        }
+
+        if (!string.IsNullOrWhiteSpace(q))
+        {
+            productsQuery = productsQuery.Where(p => p.Name.Contains(q) || p.Description.Contains(q));
+        }
+
+        var products = await productsQuery.OrderBy(p => p.BusinessTypeId).ThenBy(p => p.Name).ToListAsync();
+
+        ViewBag.BusinessTypes = businessTypes;
+        ViewBag.SelectedCategory = category;
+        ViewBag.SearchTerm = q;
+
+        return View(products);
     }
 
     public IActionResult About() => View();
